9e6496aa7a62db7632cf98d0dc1f665959c77174
agilles1 <adam.g.tpt@gmail.com>
2020-12-10 09:19:18 -0500

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,10 @@
+class Song < ApplicationRecord
+    validates :title, presence: true, uniqueness: { scope: [:release_year, :artist_name], }
+    validates :released, inclusion: { in: [true, false] }
+    validates :release_year, presence: true, if: :released, numericality: { only_integer: true, less_than_or_equal_to: Date.current.year }
+    validates :artist_name, presence: true
+    validates :genre, presence: true
+
+    
+end
+
